295ae11225f3a32670a585a88acb637f5a6843f0
Yasshi-cookie <y.teruyacookie@gmail.com>
2022-02-08 23:30:04 +0900

    トップページ追加

--- backend/app/Http/Controllers/TopController.php
+++ backend/app/Http/Controllers/TopController.php
@@ -3,46 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Math\RationalMath;
 
 class TopController extends Controller
 {
-    /**
-     * index
-     *
-     * @return void
-     */
     public function index()
     {
         return view('top.index');
-    }
-
-    /**
-     * フォーム送信時の処理
-     *
-     * @param Request $request
-     * @return void
-     */
-    public function post(Request $request)
-    {
-        $rat_a = new RationalMath($request->denominator_a, $request->numerator_a);
-        $rat_b = new RationalMath($request->denominator_b, $request->numerator_b);
-
-        switch ($request->operator) {
-            case 1:
-                $img_url = RationalMath::add_rat($rat_a, $rat_b)->display();
-                break;
-            case 2:
-                $img_url = RationalMath::sub_rat($rat_a, $rat_b)->display();
-                break;
-            case 3:
-                $img_url = RationalMath::product_rat($rat_a, $rat_b)->display();
-                break;
-            case 4:
-                $img_url = RationalMath::devide_rat($rat_a, $rat_b)->display();
-                break;
-        }
-
-        return view('top.index', compact('img_url'));
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\TopController::class, 'index']);
-Route::post('/', [App\Http\Controllers\TopController::class, 'post']);
+Route::get('/', [App\Http\Controllers\TopController::class, 'index'])->name('top');
