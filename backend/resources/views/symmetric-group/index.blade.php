55769ea69e226454847872dc3c49c91cb60bc291
Yasshi-cookie <y.teruyacookie@gmail.com>
2022-02-20 23:31:03 +0900

    あみだくじの横線の表現方法を決めた

--- backend/resources/views/symmetric-group/index.blade.php
+++ backend/resources/views/symmetric-group/index.blade.php
@@ -1,74 +1,66 @@
 @extends('layouts.default')
 
 @section('title', 'あみだくじ')
-
 @section('content')
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <title></title>
-    <style>
-      /* html {
-        height: 90%;
-        width: 100%;
-      }
-      body {
-        height: 100%;
-      }
-      div {
-        height: 100%;
-        padding: 10px;
-      } */
-      table{
-        width: 100%;
-        height: 150px;
-        border-collapse: collapse;
-        /* border: 2px solid #000; */
-        font-size: 12px;
-      }
-      td {
-        padding: 10px;
-        border: 2px solid #000;
-      }
-    </style>
-  </head>
-  <body>
-    <div>
-      <table summary="サンプル" rules="cols">
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </body>
-</html>
+<?php
+  // TODO: スタイルシートを別ファイル切り分ける
+  // MEMO: ブラウザのクリックイベントに対して、横線の表示/非表示を切り替え
+  // MEMO: formを作成。クリックイベントごとにフォームのバリューを更新
+?>
+<style>
+  table {
+    width: 100%;
+    height: 150px;
+    border-collapse: collapse;
+    font-size: 12px;
+  }
+  td {
+    padding: 10px;
+    border: 2px solid #000;
+  }
+  .start, .end {
+    border-width: 0px 0px 0px 0px;
+  }
+  .second {
+    border-width: 0px 2px 0px 2px;
+  }
+  .second-target {
+    border-width: 0px 2px 2px 2px;
+  }
+</style>
 
+<div>
+  <table summary="サンプル" rules="cols">
+    <tbody>
+      <tr>
+        <td class="start"></td>
+        <td class="second"></td>
+        <td></td>
+        <td></td>
+        <td class="end"></td>
+      </tr>
+      <tr>
+        <td class="start"></td>
+        <td class="second-target"></td>
+        <td></td>
+        <td></td>
+        <td class="end"></td>
+      </tr>
+      <tr>
+        <td class="start"></td>
+        <td class="second"></td>
+        <td></td>
+        <td></td>
+        <td class="end"></td>
+      </tr>
+      <tr>
+        <td class="start"></td>
+        <td class="second"></td>
+        <td></td>
+        <td></td>
+        <td class="end"></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 @endsection
